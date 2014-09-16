75e5de31e1f96525c13ffbc9da02eb0ca10ec50f
kmiya <kmiya@outlook.com>
2014-09-16 23:40:51 +0900

    solved exercises before Options

--- ex_Lists.v
+++ ex_Lists.v
@@ -286,3 +286,286 @@ Proof. reflexivity. Qed.
 (*   beq_nat a b = true -> count b (add a s) = 1 + count b s. *)
 (* Proof. Abort. *)
 (** [] *)
+
+(** * Reasoning About Lists *)
+
+Theorem nil_app : forall l:natlist,
+  [] ++ l = l.
+Proof. reflexivity. Qed.
+
+Theorem tl_length_pred : forall l:natlist,
+  pred (length l) = length (tl l).
+Proof.
+  intros l. destruct l as [| n l'].
+  Case "l = nil".
+    reflexivity.
+  Case "l = cons n l'".
+    reflexivity.  Qed.
+
+(** ** Micro-Sermon *)
+
+(** ** Induction on Lists *)
+
+Theorem app_assoc : forall l1 l2 l3 : natlist,
+  (l1 ++ l2) ++ l3 = l1 ++ (l2 ++ l3).
+Proof.
+  intros l1 l2 l3. induction l1 as [| n l1'].
+  Case "l1 = nil".
+    reflexivity.
+  Case "l1 = cons n l1'".
+    simpl. rewrite -> IHl1'. reflexivity.  Qed.
+
+(** *** Informal version *)
+
+(** *** Another example *)
+
+Theorem app_length : forall l1 l2 : natlist,
+  length (l1 ++ l2) = (length l1) + (length l2).
+Proof.
+  (* WORKED IN CLASS *)
+  intros l1 l2. induction l1 as [| n l1'].
+  Case "l1 = nil".
+    reflexivity.
+  Case "l1 = cons".
+    simpl. rewrite -> IHl1'. reflexivity.  Qed.
+
+(** *** Reversing a list *)
+
+Fixpoint snoc (l:natlist) (v:nat) : natlist :=
+  match l with
+  | nil    => [v]
+  | h :: t => h :: (snoc t v)
+  end.
+
+Fixpoint rev (l:natlist) : natlist :=
+  match l with
+  | nil    => nil
+  | h :: t => snoc (rev t) h
+  end.
+
+(** *** Proofs about reverse *)
+
+Theorem length_snoc : forall n : nat, forall l : natlist,
+  length (snoc l n) = S (length l).
+Proof.
+  intros n l. induction l as [| n' l'].
+  Case "l = nil".
+    reflexivity.
+  Case "l = cons n' l'".
+    simpl. rewrite -> IHl'. reflexivity.  Qed.
+
+Theorem rev_length : forall l : natlist,
+  length (rev l) = length l.
+Proof.
+  intros l. induction l as [| n l'].
+  Case "l = nil".
+    reflexivity.
+  Case "l = cons".
+    simpl. rewrite -> length_snoc.
+    rewrite -> IHl'. reflexivity.  Qed.
+
+(** ** List Exercises, Part 1 *)
+
+(** **** Exercise: 3 stars (list_exercises) *)
+(** More practice with lists. *)
+
+Theorem app_nil_end : forall l : natlist,
+  l ++ [] = l.
+Proof.
+  intros l. induction l as [| x xs].
+  Case "l = []". reflexivity.
+  Case "l = cons".
+    simpl. rewrite -> IHxs. reflexivity. Qed.
+
+Theorem rev_snoc : forall (v : nat) (l : natlist),
+  rev (snoc l v) = v :: rev l.
+Proof.
+  intros v l. induction l as [| x xs].
+  Case "l = []".
+    reflexivity.
+  Case "l = cons".
+    simpl. rewrite -> IHxs. reflexivity. Qed.
+
+Theorem rev_involutive : forall l : natlist,
+  rev (rev l) = l.
+Proof.
+  intros l. induction l as [| x xs].
+  Case "l = []". reflexivity.
+  Case "l = cons".
+    simpl. rewrite -> rev_snoc. rewrite -> IHxs. reflexivity. Qed.
+
+(* ref: https://github.com/etosch/software_foundations/blob/master/lesson3_Lists.v *)
+
+(** There is a short solution to the next exercise.  If you find
+    yourself getting tangled up, step back and try to look for a
+    simpler way. *)
+
+Theorem app_assoc4 : forall l1 l2 l3 l4 : natlist,
+  l1 ++ (l2 ++ (l3 ++ l4)) = ((l1 ++ l2) ++ l3) ++ l4.
+Proof.
+  intros l1 l2 l3 l4.
+  replace ((l1 ++ l2) ++ l3) with (l1 ++ l2 ++ l3).
+  rewrite -> app_assoc.
+  rewrite -> app_assoc. reflexivity.
+  Case "replace".
+    rewrite -> app_assoc. reflexivity. Qed.
+
+Theorem snoc_append : forall (l : natlist) (n : nat),
+  snoc l n = l ++ [n].
+Proof.
+  intros l n. induction l as [| x xs].
+  Case "l = []".
+    reflexivity.
+  Case "l = cons".
+    simpl. rewrite -> IHxs. reflexivity. Qed.
+
+Theorem distr_rev : forall l1 l2 : natlist,
+  rev (l1 ++ l2) = (rev l2) ++ (rev l1).
+Proof.
+  intros l1 l2. induction l1 as [| x xs].
+  Case "l1 = []".
+    simpl. rewrite -> app_nil_end. reflexivity.
+  Case "l1 = cons".
+    simpl. rewrite -> IHxs.
+    rewrite -> snoc_append. rewrite -> snoc_append.
+    rewrite -> app_assoc. reflexivity. Qed.
+
+(** An exercise about your implementation of [nonzeros]: *)
+
+Lemma nonzeros_nil : forall l : natlist,
+  nonzeros [] = [].
+Proof.
+  reflexivity. Qed.
+
+Lemma nonzeros_app : forall l1 l2 : natlist,
+  nonzeros (l1 ++ l2) = (nonzeros l1) ++ (nonzeros l2).
+Proof.
+  intros l1 l2. induction l1 as [| x xs].
+  Case "l1 = []".
+    reflexivity.
+  Case "l1 = cons".
+    destruct x as [| x'].
+    SCase "x = 0".
+      simpl. rewrite -> IHxs. reflexivity.
+    SCase "x > 0".
+      simpl. rewrite -> IHxs. reflexivity.
+Qed.
+(** [] *)
+
+(** **** Exercise: 2 stars (beq_natlist) *)
+(** Fill in the definition of [beq_natlist], which compares
+    lists of numbers for equality.  Prove that [beq_natlist l l]
+    yields [true] for every list [l]. *)
+
+Fixpoint beq_natlist (l1 l2 : natlist) : bool :=
+  match l1 with
+    | nil => match l2 with
+               | nil => true
+               | _   => false
+             end
+    | h :: t => match l2 with
+               | nil => false
+               | h2 :: t2 => match beq_nat h h2 with
+                               | false => false
+                               | true  => beq_natlist t t2
+                             end
+                end
+  end.
+
+Example test_beq_natlist1 :   (beq_natlist nil nil = true).
+Proof. reflexivity. Qed.
+Example test_beq_natlist2 :   beq_natlist [1;2;3] [1;2;3] = true.
+Proof. reflexivity. Qed.
+Example test_beq_natlist3 :   beq_natlist [1;2;3] [1;2;4] = false.
+Proof. reflexivity. Qed.
+
+Lemma beq_n_n : forall n : nat,
+  beq_nat n n = true.
+Proof.
+  intros. induction n as [| n'].
+  Case "n = 0".
+    reflexivity.
+  Case "n > 0".
+    simpl. rewrite -> IHn'. reflexivity. Qed.
+
+Theorem beq_natlist_refl : forall l : natlist,
+  true = beq_natlist l l.
+Proof.
+  intro l. induction l as [| x xs].
+  Case "l = []".
+    reflexivity.
+  Case "l = cons".
+    simpl. rewrite -> beq_n_n.
+    rewrite <- IHxs. reflexivity.
+Qed.
+
+(** ** List Exercises, Part 2 *)
+
+(** **** Exercise: 2 stars (list_design) *)
+(** Design exercise:
+     - Write down a non-trivial theorem involving [cons]
+       ([::]), [snoc], and [app] ([++]).
+     - Prove it. *)
+
+(* TODO *)
+(** [] *)
+
+(** **** Exercise: 3 stars, advanced (bag_proofs) *)
+(** Here are a couple of little theorems to prove about your
+    definitions about bags earlier in the file. *)
+
+Theorem count_member_nonzero : forall (s : bag),
+  ble_nat 1 (count 1 (1 :: s)) = true.
+Proof.
+  reflexivity.
+Qed.
+
+(** The following lemma about [ble_nat] might help you in the next proof. *)
+
+Theorem ble_n_Sn : forall n,
+  ble_nat n (S n) = true.
+Proof.
+  intros n. induction n as [| n'].
+  Case "0".
+    simpl.  reflexivity.
+  Case "S n'".
+    simpl. rewrite IHn'. reflexivity. Qed.
+
+Theorem remove_decreases_count: forall (s : bag),
+  ble_nat (count 0 (remove_one 0 s)) (count 0 s) = true.
+Proof.
+  intros s. induction s as [| h t].
+  Case "s = []".
+    reflexivity.
+  Case "s = cons".
+    destruct h as [| h']. simpl.
+    SCase "h = 0".
+      rewrite -> ble_n_Sn. reflexivity.
+    SCase "h > 0".
+    simpl. rewrite -> IHt. reflexivity.
+Qed.
+
+(** **** Exercise: 3 stars, optional (bag_count_sum) *)
+(** Write down an interesting theorem about bags involving the
+    functions [count] and [sum], and prove it.*)
+
+(* TODO *)
+(** [] *)
+
+(** **** Exercise: 4 stars, advanced (rev_injective) *)
+(** Prove that the [rev] function is injective, that is,
+
+    forall (l1 l2 : natlist), rev l1 = rev l2 -> l1 = l2.
+
+There is a hard way and an easy way to solve this exercise.
+*)
+
+Theorem rev_injective: forall (l1 l2 : natlist),
+  rev l1 = rev l2 -> l1 = l2.
+Proof.
+  intros l1 l2 H.
+  rewrite <- rev_involutive.
+  rewrite <- H.
+  rewrite -> rev_involutive.
+  reflexivity.
+Qed.
