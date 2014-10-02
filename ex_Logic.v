6d4232e779953b03b9e7da394aa04ef47c68d0a4
kmiya <kmiya@outlook.com>
2014-10-03 00:15:55 +0900

    solved exercises before the advanced topic

--- ex_Logic.v
+++ ex_Logic.v
@@ -1,1 +1,170 @@
+(** * Logic: Logic in Coq *)
+
 Require Export MoreCoq.
+
+(** * Proofs and Evidence *)
+
+(** ** Implications _are_ functions *)
+
+(** * Conjunction (Logical "and") *)
+
+Inductive and (P Q : Prop) : Prop :=
+  conj : P -> Q -> (and P Q).
+
+Notation "P /\ Q" := (and P Q) : type_scope.
+
+(** ** "Eliminating" conjunctions *)
+
+Theorem proj1 : forall P Q : Prop,
+  P /\ Q -> P.
+Proof.
+  intros P Q H.
+  inversion H as [HP HQ].
+  apply HP.  Qed.
+
+(** **** Exercise: 1 star, optional (proj2) *)
+Theorem proj2 : forall P Q : Prop,
+  P /\ Q -> Q.
+Proof.
+  intros P Q H.
+  inversion H as [HP HQ].
+  apply H.
+Qed.
+
+Theorem and_commut : forall P Q : Prop,
+  P /\ Q -> Q /\ P.
+Proof.
+  (* WORKED IN CLASS *)
+  intros P Q H.
+  inversion H as [HP HQ].
+  split.
+    Case "left". apply HQ.
+    Case "right". apply HP.  Qed.
+
+
+(** **** Exercise: 2 stars (and_assoc) *)
+(** In the following proof, notice how the _nested pattern_ in the
+    [inversion] breaks the hypothesis [H : P /\ (Q /\ R)] down into
+    [HP: P], [HQ : Q], and [HR : R].  Finish the proof from there: *)
+
+Theorem and_assoc : forall P Q R : Prop,
+  P /\ (Q /\ R) -> (P /\ Q) /\ R.
+Proof.
+  intros P Q R H.
+  inversion H as [HP [HQ HR]].
+  split.
+    split.
+      apply HP.
+      apply HQ.
+    apply HR.
+Qed.
+
+(** * Iff *)
+
+Definition iff (P Q : Prop) := (P -> Q) /\ (Q -> P).
+
+Notation "P <-> Q" := (iff P Q)
+                      (at level 95, no associativity)
+                      : type_scope.
+
+Theorem iff_implies : forall P Q : Prop,
+  (P <-> Q) -> P -> Q.
+Proof.
+  intros P Q H.
+  inversion H as [HAB HBA]. apply HAB.  Qed.
+
+Theorem iff_sym : forall P Q : Prop,
+  (P <-> Q) -> (Q <-> P).
+Proof.
+  (* WORKED IN CLASS *)
+  intros P Q H.
+  inversion H as [HAB HBA].
+  split.
+    Case "->". apply HBA.
+    Case "<-". apply HAB.  Qed.
+
+(** **** Exercise: 1 star, optional (iff_properties) *)
+(** Using the above proof that [<->] is symmetric ([iff_sym]) as
+    a guide, prove that it is also reflexive and transitive. *)
+
+Theorem iff_refl : forall P : Prop,
+  P <-> P.
+Proof.
+  intros P.
+  split.
+    intros HP. apply HP.
+    intros HP. apply HP.
+Qed.
+
+Theorem iff_trans : forall P Q R : Prop,
+  (P <-> Q) -> (Q <-> R) -> (P <-> R).
+Proof.
+  intros P Q R HPQ HQR.
+  inversion HPQ as [HAB HBA].
+  inversion HQR as [HBC HCB].
+  split.
+    Case "P -> R".
+      intros HP.
+      apply HAB in HP. apply HBC. apply HP.
+    Case "R -> P".
+      intros HR.
+      apply HCB in HR. apply HBA. apply HR.
+Qed.
+(** Hint: If you have an iff hypothesis in the context, you can use
+    [inversion] to break it into two separate implications.  (Think
+    about why this works.) *)
+
+(** * Disjunction (Logical "or") *)
+
+(** ** Implementing Disjunction *)
+
+Inductive or (P Q : Prop) : Prop :=
+  | or_introl : P -> or P Q
+  | or_intror : Q -> or P Q.
+
+Notation "P \/ Q" := (or P Q) : type_scope.
+
+Theorem or_commut : forall P Q : Prop,
+  P \/ Q  -> Q \/ P.
+Proof.
+  intros P Q H.
+  inversion H as [HP | HQ].
+    Case "left". apply or_intror. apply HP.
+    Case "right". apply or_introl. apply HQ.  Qed.
+
+Theorem or_distributes_over_and_1 : forall P Q R : Prop,
+  P \/ (Q /\ R) -> (P \/ Q) /\ (P \/ R).
+Proof.
+  intros P Q R. intros H. inversion H as [HP | [HQ HR]].
+    Case "left". split.
+      SCase "left". left. apply HP.
+      SCase "right". left. apply HP.
+    Case "right". split.
+      SCase "left". right. apply HQ.
+      SCase "right". right. apply HR.  Qed.
+
+(** **** Exercise: 2 stars (or_distributes_over_and_2) *)
+Theorem or_distributes_over_and_2 : forall P Q R : Prop,
+  (P \/ Q) /\ (P \/ R) -> P \/ (Q /\ R).
+Proof.
+  intros P Q R H.
+  inversion H as [[HP1 | HQ] [HP2 | HR]].
+  Case "P1 and P2".
+    left. apply HP1.
+  Case "P1 and R".
+    left. apply HP1.
+  Case "Q and P2".
+    left. apply HP2.
+  Case "Q and R".
+    right. split. apply HQ. apply HR.
+Qed.
+
+(** **** Exercise: 1 star, optional (or_distributes_over_and) *)
+Theorem or_distributes_over_and : forall P Q R : Prop,
+  P \/ (Q /\ R) <-> (P \/ Q) /\ (P \/ R).
+Proof.
+  intros P Q R.
+  split.
+    Case "->". apply or_distributes_over_and_1.
+    Case "<-". apply or_distributes_over_and_2.
+Qed.
